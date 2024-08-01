f874a576a3d4f5f4a27b8ad8db0e7513fd5044ff
crono628 <84817352+crono628@users.noreply.github.com>
2024-07-31 20:18:32 -0400

    chore: Refactor App component to use Material-UI Paper for styling. logic working.

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import SearchBox from './components/SearchBox'
+import { Paper } from '@mui/material'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <SearchBox />
-    </>
+    <div>
+      <Paper
+        elevation={7}
+        style={{
+          padding: 20,
+          margin: 20,
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center'
+        }}
+      >
+        <SearchBox />
+      </Paper>
+    </div>
   )
 }
 
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,19 +4,35 @@ import TextField from '@mui/material/TextField'
 import { tables } from '../data/guestList'
 
 const SearchBox = () => {
-  const handleSearch = (event, value) => {
-    console.log(value)
+  // create an array of unique names from tables object
+  function transformTablesToOptions(tables) {
+    const nameSet = new Set()
+    for (const table in tables) {
+      if (tables.hasOwnProperty(table)) {
+        tables[table].forEach((name) => {
+          nameSet.add(name)
+        })
+      }
+    }
+    // Convert the Set to an array of objects with a label property
+    return Array.from(nameSet).map((name) => ({ label: name }))
   }
 
+  const options = transformTablesToOptions(tables)
+  console.log(options)
+
   return (
-    <Autocomplete
-      options={tables}
-      getOptionLabel={(option) => option.name}
-      renderInput={(params) => (
-        <TextField {...params} label="Search" variant="outlined" />
-      )}
-      onChange={handleSearch}
-    />
+    <>
+      <Autocomplete
+        id="search-box"
+        options={options}
+        getOptionLabel={(option) => option.label}
+        style={{ width: 300 }}
+        renderInput={(params) => (
+          <TextField {...params} label="Search" variant="outlined" />
+        )}
+      />
+    </>
   )
 }
 export default SearchBox
