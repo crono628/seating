eee8deca5d73ae56b9a01bcda15be8f60d53625d
crono628 <84817352+crono628@users.noreply.github.com>
2024-07-31 20:52:32 -0400

    chore: Refactor App component to use Material-UI Paper for styling and add SearchBox component for searching guests

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,49 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import SearchBox from './components/SearchBox'
+import React, { useState } from 'react'
+import SearchBox from './components/SearchBox' // Adjust the import path as necessary
 import { Paper } from '@mui/material'
+import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  const [selectedName, setSelectedName] = useState(null)
+
+  const handleSelectionChange = (newValue) => {
+    setSelectedName(newValue)
+  }
 
   return (
-    <div>
-      <Paper
-        elevation={7}
-        style={{
-          padding: 20,
-          margin: 20,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
-        }}
-      >
-        <SearchBox />
-      </Paper>
-    </div>
+    <Paper
+      elevation={7}
+      style={{
+        padding: 20,
+        margin: 20,
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center'
+      }}
+    >
+      {selectedName && (
+        <div>
+          <h2>Welcome, {selectedName.name}</h2>
+        </div>
+      )}
+      {!selectedName && <SearchBox onSelectionChange={handleSelectionChange} />}
+      {selectedName && (
+        <div>
+          <p>You are seated at {selectedName.table}</p>
+        </div>
+      )}
+    </Paper>
+
+    // <div>
+    //   <h1>Guest List</h1>
+    //   <SearchBox onSelectionChange={handleSelectionChange} />
+    //   {selectedName && (
+    //     <div>
+    //       <h2>Selected Name</h2>
+    //       <p>{selectedName.table}</p>
+    //     </div>
+    //   )}
+    // </div>
   )
 }
 
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -3,7 +3,7 @@ import Autocomplete from '@mui/material/Autocomplete'
 import TextField from '@mui/material/TextField'
 import { tables } from '../data/guestList'
 
-const SearchBox = () => {
+const SearchBox = ({ onSelectionChange }) => {
   // create an array of unique names from tables object
   function transformTablesToOptions(tables) {
     const nameSet = new Set()
@@ -20,19 +20,30 @@ const SearchBox = () => {
 
   const options = transformTablesToOptions(tables)
   console.log(options)
-
   return (
-    <>
-      <Autocomplete
-        id="search-box"
-        options={options}
-        getOptionLabel={(option) => option.label}
-        style={{ width: 300 }}
-        renderInput={(params) => (
-          <TextField {...params} label="Search" variant="outlined" />
-        )}
-      />
-    </>
+    <Autocomplete
+      id="search-box"
+      options={options}
+      getOptionLabel={(option) => option.label}
+      style={{ width: 300, backgroundColor: 'white' }}
+      renderInput={(params) => (
+        <TextField {...params} label="Search" variant="outlined" />
+      )}
+      onChange={(event, newValue) =>
+        onSelectionChange(
+          //store name and table in an object
+          newValue
+            ? {
+                name: newValue.label,
+                table: Object.keys(tables).find((table) =>
+                  tables[table].includes(newValue.label)
+                )
+              }
+            : null
+        )
+      }
+    />
   )
 }
+
 export default SearchBox
