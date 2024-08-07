84056cc2e306d4b58fc303eddc75598f19a9e1e2
crono628 <84817352+crono628@users.noreply.github.com>
2024-08-07 19:06:00 -0400

    updated guest list, added new search button

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import SearchBox from './components/SearchBox' // Adjust the import path as necessary
-import { Card, Paper } from '@mui/material'
+import { Button, Card, Link, Paper } from '@mui/material'
 import './App.css'
 import qrCode from './assets/seatLocator.png'
+import './firebase'
 
 const App = () => {
   const [selectedName, setSelectedName] = useState(null)
@@ -12,66 +13,82 @@ const App = () => {
   }
 
   return (
-    <Card>
-      <Paper
-        elevation={22}
-        style={{
-          padding: 20,
-          margin: 30,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center'
-        }}
-      >
-        {selectedName && (
-          <div>
-            <h2>
-              Welcome, <div>{selectedName.name}</div>
-            </h2>
-          </div>
-        )}
-        {!selectedName && (
-          <div>
-            <p>
-              Please begin typing your name below and <strong>select</strong>{' '}
-              your name to locate your table
-            </p>
-            <SearchBox onSelectionChange={handleSelectionChange} />
-          </div>
-        )}
-        {selectedName && (
-          <div>
-            <p>You are seated at {selectedName.table}</p>
-          </div>
-        )}
-        {selectedName && (
-          <div>
-            <img
-              style={{
-                width: '160px',
-                height: '160px',
-                margin: '20px'
-              }}
-              src={qrCode}
-              alt="Seat Locator"
-            />
-            <p>Share this QR code with others to help them find their table</p>
-            <p>Pull down on this screen to start a new search</p>
-          </div>
-        )}
-      </Paper>
-    </Card>
-
-    // <div>
-    //   <h1>Guest List</h1>
-    //   <SearchBox onSelectionChange={handleSelectionChange} />
-    //   {selectedName && (
-    //     <div>
-    //       <h2>Selected Name</h2>
-    //       <p>{selectedName.table}</p>
-    //     </div>
-    //   )}
-    // </div>
+    <>
+      <Card>
+        <Paper
+          elevation={22}
+          style={{
+            padding: 20,
+            margin: 30,
+            display: 'flex',
+            flexDirection: 'column',
+            alignItems: 'center'
+          }}
+        >
+          {selectedName && (
+            <div>
+              <h2>
+                Welcome, <div>{selectedName.name}</div>
+              </h2>
+            </div>
+          )}
+          {!selectedName && (
+            <div>
+              <p>
+                Please begin typing your name below and <strong>select</strong>{' '}
+                your name to locate your table
+              </p>
+              <SearchBox onSelectionChange={handleSelectionChange} />
+            </div>
+          )}
+          {selectedName && (
+            <div>
+              <p>You are seated at table {selectedName.table}</p>
+            </div>
+          )}
+          {selectedName && (
+            <div>
+              <img
+                style={{
+                  width: '160px',
+                  height: '160px',
+                  margin: '20px'
+                }}
+                src={qrCode}
+                alt="Seat Locator"
+              />
+              <p>
+                Share this QR code with others to help them find their table
+              </p>
+              <Button
+                variant="outlined"
+                sx={{
+                  color: 'inherit',
+                  borderColor: 'inherit',
+                  backgroundColor: 'inherit'
+                }}
+                onClick={
+                  () => setSelectedName(null) // Reset the selected name
+                }
+              >
+                New Search
+              </Button>
+            </div>
+          )}
+        </Paper>
+      </Card>
+      <footer className="footer">
+        <Link
+          href="https://michaeldesantis.com/"
+          target="_blank"
+          rel="noopener"
+          underline="none"
+          sx={{ color: 'inherit', textDecoration: 'none' }}
+        >
+          md ©2024
+        </Link>
+      </footer>
+    </>
   )
 }
 
--- src/components/SearchBox.jsx
+++ src/components/SearchBox.jsx
@@ -4,27 +4,12 @@ import TextField from '@mui/material/TextField'
 import { tables } from '../data/guestList'
 
 const SearchBox = ({ onSelectionChange }) => {
-  // create an array of unique names from tables object
-  function transformTablesToOptions(tables) {
-    const nameSet = new Set()
-    for (const table in tables) {
-      if (tables.hasOwnProperty(table)) {
-        tables[table].forEach((name) => {
-          nameSet.add(name)
-        })
-      }
-    }
-    // Convert the Set to an array of objects with a label property
-    return Array.from(nameSet).map((name) => ({ label: name }))
-  }
-
-  const options = transformTablesToOptions(tables)
-  console.log(options)
+  const options = tables
   return (
     <Autocomplete
       id="search-box"
       options={options}
-      getOptionLabel={(option) => option.label}
+      getOptionLabel={(option) => option.name}
       style={{ width: '100%', backgroundColor: 'white' }}
       renderInput={(params) => (
         <TextField
@@ -38,10 +23,8 @@ const SearchBox = ({ onSelectionChange }) => {
           //store name and table in an object
           newValue
             ? {
-                name: newValue.label,
-                table: Object.keys(tables).find((table) =>
-                  tables[table].includes(newValue.label)
-                )
+                name: newValue.name,
+                table: newValue.table
               }
             : null
         )
